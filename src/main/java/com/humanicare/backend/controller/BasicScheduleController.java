bc52d5e7df5c76ad1ed76a9f39016b3256a9e18e
seokjin1023 <150601054+seokjin1023@users.noreply.github.com>
2025-03-25 20:04:31 +0900

    feat : BasicSchedule 의 내부 로직(Service) 구현 (#6)

--- src/main/java/com/humanicare/backend/controller/BasicScheduleController.java
+++ src/main/java/com/humanicare/backend/controller/BasicScheduleController.java
@@ -4,8 +4,11 @@ import com.humanicare.backend.apiPayload.ApiResponse;
 import com.humanicare.backend.apiPayload.code.status.SuccessStatus;
 import com.humanicare.backend.converter.BasicScheduleConverter;
 import com.humanicare.backend.domain.BasicSchedule;
+import com.humanicare.backend.domain.oauth.User;
 import com.humanicare.backend.dto.BasicScheduleDto;
+import com.humanicare.backend.jwt.service.JwtService;
 import com.humanicare.backend.service.BasicScheduleService;
+import com.humanicare.backend.service.user.UserCheckService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -30,10 +33,10 @@ public class BasicScheduleController {
                 BasicScheduleConverter.toScheduleDtoList(schedules));
     }
 
-    @GetMapping("/basic-schedules/{id}")
+    @GetMapping("/basic-schedules")
     @Operation(summary = "하나의 기본 일정 가져오기")
     public ApiResponse<BasicScheduleDto.ScheduleDto> getBasicSchedule(@RequestHeader("Authorization") final String authorizationHeader,
-                                                                      @PathVariable Long id) {
+                                                                      @RequestParam("scheduleId") final Long id) {
         BasicSchedule schedule = basicScheduleService.getSchedule(id);
         return ApiResponse.of(SuccessStatus.GET_BASIC_SCHEDULE,
                 BasicScheduleConverter.toBasicScheduleDto(schedule));
@@ -43,23 +46,26 @@ public class BasicScheduleController {
     @Operation(summary = "기본 일정 생성하기")
     public ApiResponse<Void> createBasicSchedule(@RequestHeader("Authorization") final String authorizationHeader,
                                                  @RequestBody BasicScheduleDto.ScheduleDto scheduleDto) {
-        basicScheduleService.createSchedule(scheduleDto);
+        String accessToken = authorizationHeader.replace(ACCESS_TOKEN_PREFIX, ACCESS_TOKEN_REPLACEMENT);
+        basicScheduleService.createSchedule(accessToken, scheduleDto);
         return ApiResponse.ofNoting(SuccessStatus.SAVE_BASIC_SCHEDULE);
     }
 
     @PutMapping("/basic-schedules")
     @Operation(summary = "기본 일정 수정하기")
     public ApiResponse<BasicScheduleDto.ScheduleDto> updateBasicSchedule(@RequestHeader("Authorization") final String authorizationHeader,
-                                                 @RequestBody BasicScheduleDto.ScheduleDto scheduleDto) {
-        basicScheduleService.updateSchedule(scheduleDto);
+                                                 @RequestBody BasicScheduleDto.ScheduleDto scheduleDto, @RequestParam("scheduleId") final Long id) {
+        String accessToken = authorizationHeader.replace(ACCESS_TOKEN_PREFIX, ACCESS_TOKEN_REPLACEMENT);
+        basicScheduleService.updateSchedule(accessToken, scheduleDto, id);
         return ApiResponse.ofNoting(SuccessStatus.PUT_BASIC_SCHEDULE);
     }
 
     @DeleteMapping("/basic-schedules")
     @Operation(summary = "기본 일정 삭제하기")
     public ApiResponse<Void> deleteBasicSchedule(@RequestHeader("Authorization") final String authorizationHeader,
-                                                 @RequestBody BasicScheduleDto.ScheduleDto scheduleDto) {
-        basicScheduleService.deleteSchedule(scheduleDto);
+                                                 @RequestParam("ScheduleId") final Long id) {
+        String accessToken = authorizationHeader.replace(ACCESS_TOKEN_PREFIX, ACCESS_TOKEN_REPLACEMENT);
+        basicScheduleService.deleteSchedule(accessToken, id);
         return ApiResponse.ofNoting(SuccessStatus.DELETE_BASIC_SCHEDULE);
     }
 }
--- src/main/java/com/humanicare/backend/service/BasicScheduleService.java
+++ src/main/java/com/humanicare/backend/service/BasicScheduleService.java
@@ -1,35 +1,71 @@
 package com.humanicare.backend.service;
 
+import com.humanicare.backend.apiPayload.code.status.ErrorStatus;
+import com.humanicare.backend.apiPayload.exception.handler.BasicScheduleHandler;
+import com.humanicare.backend.converter.BasicScheduleConverter;
 import com.humanicare.backend.domain.BasicSchedule;
+import com.humanicare.backend.domain.oauth.User;
 import com.humanicare.backend.dto.BasicScheduleDto;
+import com.humanicare.backend.repository.BasicScheduleRepository;
+import com.humanicare.backend.service.user.UserCheckService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class BasicScheduleService {
 
+    private final BasicScheduleRepository basicScheduleRepository;
+    private final UserCheckService userCheckService;
+
+    //BasicSchedule의 User id와 현재 로그인된 User의 id가 일치하지 않으면 Forbidden
+    private void checkValidUser(BasicSchedule schedule, User currentUser) {
+        if (!schedule.getUser().getId().equals(currentUser.getId())) {
+            throw new BasicScheduleHandler(ErrorStatus._FORBIDDEN);
+        }
+    }
+
     public List<BasicSchedule> getAllSchedule() {
-        return new ArrayList<>();
+        return basicScheduleRepository.findAll();
     }
 
 
     public BasicSchedule getSchedule(final Long id) {
-        return BasicSchedule.builder().build();
+        Optional<BasicSchedule> schedule = basicScheduleRepository.findById(id);
+        if(schedule.isPresent()) {
+            return schedule.get();
+        } else {
+            throw new BasicScheduleHandler(ErrorStatus._BASIC_SCHEDULE_NOT_FOUND);
+        }
     }
 
-    public void createSchedule(BasicScheduleDto.ScheduleDto scheduleDto) {
-
+    public void createSchedule(String accessToken, BasicScheduleDto.ScheduleDto scheduleDto) {
+        User user = userCheckService.getUserByToken(accessToken);
+        basicScheduleRepository.save(BasicScheduleConverter.toBasicSchedule(user, scheduleDto));
     }
 
-    public void updateSchedule(BasicScheduleDto.ScheduleDto scheduleDto) {
+    public void updateSchedule(String accessToken, BasicScheduleDto.ScheduleDto scheduleDto, Long id) {
+        User user = userCheckService.getUserByToken(accessToken);
+        BasicSchedule original = basicScheduleRepository.findById(id)
+                .orElseThrow(() -> new BasicScheduleHandler(ErrorStatus._BASIC_SCHEDULE_NOT_FOUND)); // ② 기존 일정 조회
 
+        checkValidUser(original, user);
+        basicScheduleRepository.save(BasicScheduleConverter.toBasicSchedule(user, scheduleDto));
     }
 
-    public void deleteSchedule(BasicScheduleDto.ScheduleDto scheduleDto) {
+    public void deleteSchedule(String accessToken, Long id) {
+        User currentUser = userCheckService.getUserByToken(accessToken);
+        BasicSchedule schedule = basicScheduleRepository.findById(id)
+                .orElseThrow(() -> new BasicScheduleHandler(ErrorStatus._BASIC_SCHEDULE_NOT_FOUND));
 
+        checkValidUser(schedule, currentUser);
+
+        basicScheduleRepository.delete(schedule);
     }
 }
