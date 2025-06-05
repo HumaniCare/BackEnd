836ccbd7638cdf26d9548996e8f670bc1347a917
seokjin1023 <babyy1023@naver.com>
2025-06-05 12:41:25 +0900

    fix: Dto annotation추가

--- src/main/java/com/humanicare/backend/dto/ReportDto.java
+++ src/main/java/com/humanicare/backend/dto/ReportDto.java
@@ -2,19 +2,20 @@ package com.humanicare.backend.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 
-@Setter
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class ReportDto {
-    @JsonProperty("imageUrl")  // JSON 필드명과 일치
+
+    @JsonProperty("imageUrl")
     private String imageUrl;
 
-    @JsonProperty("report_text")  // snake_case 대응
+    @JsonProperty("report_text")
     private String reportText;
 }
