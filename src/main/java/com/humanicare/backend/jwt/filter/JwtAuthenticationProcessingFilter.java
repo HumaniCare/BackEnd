9c2996ec181dab9ed6fbe23056dab1b9dc33f7c1
sjpark1023 <babyy1023@naver.com>
2025-06-10 22:49:03 +0900

    fix: report jwt token없이 작동

--- src/main/java/com/humanicare/backend/jwt/filter/JwtAuthenticationProcessingFilter.java
+++ src/main/java/com/humanicare/backend/jwt/filter/JwtAuthenticationProcessingFilter.java
@@ -39,7 +39,7 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
     );
 
     private static final Set<String> NOT_APPLY_JWT_FILTER_PREFIXES = Set.of(
-            "/api/spring/reissue", "/api/spring/oauth", "/api/spring/google-login", "/health"
+            "/api/spring/reissue", "/api/spring/oauth", "/api/spring/google-login", "/api/spring/report", "/health"
     );
 
     private final JwtService jwtService;
